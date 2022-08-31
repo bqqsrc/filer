4a3141ecd8c8cef6f37b004348bfc5acd38ed03e
WangXiaojie01 <1259488569@qq.com>
2022-08-31 21:03:47 +0800

    提交代码

--- filer.go
+++ filer.go
@@ -3,8 +3,11 @@ package filer
 import (
 	"os"
 	"path"
+	"github.com/bqqsrc/errer"
 )
-
+func callError(caller, format string, args ...interface{}) error {
+	return errer.CallerErrNew("filer", caller, format, args...)
+}
 func PathExist(path string) bool {
 	_, err := os.Lstat(path)
 	return !os.IsNotExist(err)
@@ -21,4 +24,26 @@ func IsFile(path string) bool {
 func JoinPathExist(args ...string) bool {
 	path := path.Join(args...)
 	return PathExist(path)
+}
+
+func ReadFile(file string) ([]byte, error) {
+	caller := "ReadFile"
+	content, err := os.ReadFile(file)
+	if err != nil {
+		return nil, callError(caller, "os.ReadFile(%s) error: %s", file, err)
+	}
+	return content, nil
+}
+
+func WriteFile(data []byte, file string) error {
+	caller := "WriteFile"
+	fi, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	defer fi.Close()
+	if err != nil {
+		return callError(caller, "os.OpenFile(%s, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644) error: %s", file, err)
+	}
+	if _, err = fi.Write(data); err != nil {
+		return callError(caller, "fi.Write error: %s", file, err)
+	} 
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/bqqsrc/filer
 
 go 1.18
+
+require(
+	github.com/bqqsrc/errer v0.0.2
+)
